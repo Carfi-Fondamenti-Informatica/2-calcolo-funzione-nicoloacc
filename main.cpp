40a0a7096e6c6b18cee21bd23c5313fc2d616d3b
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-03-30 21:14:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+    float a=0, b=0, x=0, y=0, z=0;
+    cout << "inserire i valori di a, b, x, y" << endl;
+    cin >> a >> b >> x >> y;
+    if (x<0 && y>0){
+        z = (a*x) - (b*y);
+    }
+    else if (x>=0 && y<=0){
+        z=(a*(x*x)) - (b*y);
+    }
+    else {
+        z=(a*x) + (b*(y*y));
+    }
+    cout << z;
    return 0;
 }
